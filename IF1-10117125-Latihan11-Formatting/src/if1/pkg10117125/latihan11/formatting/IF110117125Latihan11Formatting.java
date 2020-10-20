709caa29aaf03382106434a2c30c87f3d89f5c6a
zulfiihzam <35595962+zulfiihzam@users.noreply.github.com>
2020-10-21 00:52:24 +0700

    Add files via upload

---
+++ IF1-10117125-Latihan11-Formatting/src/if1/pkg10117125/latihan11/formatting/IF110117125Latihan11Formatting.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10117125.latihan11.formatting;
+
+/**
+ *
+ * @author 
+ * Nama  : Zulfi Ihzam Rahmat
+ * Kelas : IF1
+ * NIM   : 10117125
+ * Deskripsi Program : 
+ */
+public class IF110117125Latihan11Formatting {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i = 3546764;
+        int iMinus = -i;
+        System.out.println("i : " + i);
+        System.out.printf("%%d : %d%n", i);
+        System.out.printf("%%10d : %10d%n", i);
+        System.out.printf("%%+10d : %+10d%n", iMinus);
+        System.out.printf("%%,10d : %,10d%n", i);
+        System.out.printf("%%-10d : %-10d%n", i);
+        double f = 5675482.982;
+        System.out.println("f : " + f);
+        System.out.printf("%%f : %f%n", f);
+        System.out.printf("%%.2f : %.2f%n", f);
+        System.out.printf("%%12.2f : %12.2f%n", f);
+        System.out.printf("%%,12.2f : %,12.2f%n", f);
+    }
+    
+}
